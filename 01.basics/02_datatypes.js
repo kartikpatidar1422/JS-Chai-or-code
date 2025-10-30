4b3a1bbcc0999785be0209f9dfbd588fcaf7482b
Kartik Patidar <kartikpatidar1422@gmail.com>
2025-10-30 22:01:22 +0530

    Create 02_datatypes.js

---
+++ 01.basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using node js , not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+console.log("Hitesh");
+
+let name = "Harsh"
+let age = 18
+let isLoggedIn = false
+
+/*  Primitive Dataypes
+
+number => 2 to power 53
+bigInt
+string => ""
+boolean => true/false
+null => standalone value ( means khali) 
+undefined => abhi value assign nhi hui h
+symbol => unique 
+*/
+
+// Object
+
+console.log(typeof("hitesh"));
+console.log(typeof null);
+console.log(typeof undefined);
+
+
