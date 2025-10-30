754c36f9c930b835f55f6897627b1e347018c659
Kartik Patidar <kartikpatidar1422@gmail.com>
2025-10-30 21:47:17 +0530

    Create 01variables.js

---
+++ 01.basics/01variables.js
@@ -0,0 +1,18 @@
+const accountId = 142225
+let accountEmail = "abc@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+accountEmail = "hgfc@gmail.com"
+accountPassword = "21342345"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+Prefer not to use var
+ because of issue in block and functional scope
+*/
