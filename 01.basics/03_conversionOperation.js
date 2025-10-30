f29a166e949dbb2e2529edf97812b6d646978ef8
Kartik Patidar <kartikpatidar1422@gmail.com>
2025-10-30 22:15:15 +0530

    Create 03_conversionOperation.js

---
+++ 01.basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "Hitesh"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let value = Number(score)
+// console.log(typeof value);
+// console.log(value); // give error not a number NaN
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1 ; false => 0
+*/
+
+let isLoggedIn = "Hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true ; 0 => false
+"" => false
+"hitesh" => true
+*/
