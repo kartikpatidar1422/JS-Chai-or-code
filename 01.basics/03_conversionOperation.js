a139e71f7b6d9a74071552935b0b251b48d7eab9
Kartik Patidar <kartikpatidar1422@gmail.com>
2025-11-01 18:19:56 +0530

    conversion operations of datatypes

--- 01.basics/03_conversionOperation.js
+++ 01.basics/03_conversionOperation.js
@@ -13,12 +13,54 @@
 true => 1 ; false => 0
 */
 
-let isLoggedIn = "Hitesh"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "Hitesh"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 /*
 1 => true ; 0 => false
 "" => false
 "hitesh" => true
 */
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*************Operations**************
+
+// let value = 3
+// let negValue = -value
+ // console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2); // modulus
+// console.log(2**2); // power
+
+// let str1 = "hello"
+// let str2 = "hitesh"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); // op => 122 if string at first then treat all as string 
+// console.log(1+2+"2"); // op => 1+2 = 3 first treat as number then concat as string
+
+// console.log( (3+4)+5%3 );
+
+// console.log(+true); // op => give 1 
+// console.log(+""); // they are tricky conversion but not to be used 
+
+// let num1,num2,num3;
+// num1 = num2 = num3 = 2+2
+// console.log(num1,num2,num2);
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
